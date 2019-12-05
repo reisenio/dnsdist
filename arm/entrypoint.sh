2a8e4f79e6b7cf8b4932201c7fe1d16d478dd4c5
reisenio <jcgaliana@gmail.com>
2019-12-05 14:45:15 +0100

    initial commit

---
+++ arm/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /data/dnsdist/conf
+
+if [ ! -f /data/dnsdist/conf/dnsdist.conf ]; then
+	cp -pR /etc/dnsdist/* /data/dnsdist/conf/
+fi
+
+
+rm -Rf /etc/dnsdist
+
+ln -s /data/dnsdist/conf /etc/dnsdist
+
+/usr/bin/dnsdist -C /etc/dnsdist/dnsdist.conf -u _dnsdist -g _dnsdist --supervised
+
